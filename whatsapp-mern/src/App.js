cd438d09251fc1f62ddac4688e788185e6a3e89a
KARAN BALAJI R <108214039+KARANBALAJIR@users.noreply.github.com>
2023-07-19 22:23:32 +0530

    Update App.js

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import Pusher from "pusher-js";
+import axios from "axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages.log(response.data);
+
+    });
+  }, []);
+
+  useEffect(() => {
+    const pusher = new Pusher("c88958e4ab9f523f31ce", {
+      cluster: "ap2",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (newMessage) => {
+       setMessages([...messages, newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        <Chat />
+        <Chat messages= {messages} />
       </div>
-      
     </div>
   );
 }
