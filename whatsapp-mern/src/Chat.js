e0004f4eda30234f141db70ac77565a538d6bae6
KARAN BALAJI R <108214039+KARANBALAJIR@users.noreply.github.com>
2023-07-19 22:25:31 +0530

    Update Chat.js

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,15 +1,36 @@
 import { AttachFile, MoreVert, SearchOutlined} from "@mui/icons-material"
 import { Avatar, IconButton } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import MicIcon from '@mui/icons-material/Mic';
+import axios from "axios";
 import "./Chat.css";
-function Chat() {
+
+function Chat({ messages }) {
+  const [input, setInput] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    
+
+    await axios.post("http://localhost:9000/messages/new", {
+      message:input,
+      name: "Karan",
+      timestamp: "Just now!",
+      received: false,
+    });
+
+    setInput("");
+  };
+
+
   return (
   <div className="chat">
     <div className="chat__header">
       <Avatar />
 
       <div className="chat__headerInfo">
-        <h3>Karan</h3>
+        <h3>Contact Name 1</h3>
         <p>Last seen at...</p>
       </div>
 
@@ -23,12 +44,40 @@ function Chat() {
         <IconButton>
           <MoreVert />
         </IconButton>
-
-
       </div>
+    </div>
+    <div className="chat__body">
+      {messages.map((message) => (
+        <p 
+        className={`chat__message ${message.received && "chat__reciever"}`}
+        >
+        <span className="chat__name">{message.name}</span>
+          {message.message}
+          <span className="chat__timestamp">{message.timestamp}</span>
+        </p>
+
+      ))}
+    </div>
+
+    <div className="chat__footer">
+       <IconButton>
+      <EmojiEmotionsIcon />
+       </IconButton>
+      <form>
+        <input 
+        value = {input} 
+        onChange={e => setInput(e.target.value)} 
+        placeholder="Type a message"
+        type="text"
+        />
+      <button onClick={sendMessage} type="submit">Send a message</button>
+      </form>
+      <IconButton> 
+      <MicIcon />
+      </IconButton>
     </div>
   </div>
   );
 }
 
-export default Chat
+export default Chat;
