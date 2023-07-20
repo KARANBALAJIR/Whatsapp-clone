68066fa3972adbd3c00383a542e86d521dd4827b
KARAN BALAJI R <108214039+KARANBALAJIR@users.noreply.github.com>
2023-07-20 10:10:14 +0530

    Update axios.js

--- whatsapp-mern/src/axios.js
+++ whatsapp-mern/src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: "http://localhost:9000",
+    baseURL: "https://whatsapp-clone-u2wl.onrender.com",
 });
  export default instance;
